5d988c17ed3a89c755cbcc086e814bf4c2c41dbd
James Toyer <james@glipho.com>
2013-02-21 19:09:49 +0000

    Provider request token now all sorted and from method implemented

--- Glipho.OAuth.Providers/RequestToken.cs
+++ Glipho.OAuth.Providers/RequestToken.cs
@@ -1,73 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DotNetOpenAuth.OAuth.ChannelElements;
-
-namespace Glipho.OAuth.Providers
+﻿namespace Glipho.OAuth.Providers
 {
+    using System;
+    using DotNetOpenAuth.OAuth.ChannelElements;
+    using DotNetOpenAuth.OAuth.Messages;
+
+    /// <summary>
+    /// Implementation of the <see cref="IServiceProviderRequestToken"/> interface.
+    /// </summary>
     public class RequestToken : IServiceProviderRequestToken
     {
-        public Uri Callback
+        /// <summary>
+        /// Gets or sets the callback associated specifically with this token, if any. 
+        /// </summary> 
+        /// <value>
+        /// The callback URI; or <c>null</c> if no callback was specifically assigned to this token.
+        /// </value>
+        public Uri Callback { get; set; }
+
+        /// <summary>
+        /// Gets the consumer key that requested this token. 
+        /// </summary>
+        public string ConsumerKey { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the version of the Consumer that requested this token. 
+        /// </summary> 
+        /// <remarks>
+        /// This property is used to determine whether a <see cref="IServiceProviderRequestToken.VerificationCode"/> must be
+        /// generated when the user authorizes the Consumer or not. 
+        /// </remarks>
+        public Version ConsumerVersion { get; set; }
+
+        /// <summary>
+        /// Gets the (local) date that this request token was first created on. 
+        /// </summary>
+        public DateTime CreatedOn { get; private set; }
+
+        /// <summary>
+        /// Gets the token itself. 
+        /// </summary>
+        public string Token { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the verifier that the consumer must include in the <see cref="AuthorizedTokenRequest"/>
+        /// message to exchange this request token for an access token. 
+        /// </summary> 
+        /// <value>
+        /// The verifier code, or <c>null</c> if none has been assigned (yet).
+        /// </value>
+        public string VerificationCode { get; set; }
+
+        /// <summary>
+        /// Create a new request token from a database request token.
+        /// </summary>
+        /// <param name="requestToken">The database request token to create a new request token from.</param>
+        /// <returns>A new request token created from the database request token.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if a parameter is null.</exception>
+        internal static IServiceProviderRequestToken FromDataRequestToken(Database.RequestToken requestToken)
         {
-            get
+            if (requestToken == null)
             {
-                throw new NotImplementedException();
+                throw new ArgumentNullException("requestToken", "requestToken is null");
             }
-            set
+
+            return new RequestToken
             {
-                throw new NotImplementedException();
-            }
+                Callback = requestToken.Callback,
+                ConsumerKey = requestToken.ConsumerKey,
+                ConsumerVersion = requestToken.ConsumerVersion,
+                CreatedOn = requestToken.Created.ToLocalTime(),
+                Token = requestToken.Token,
+                VerificationCode = requestToken.VerificationCode,
+            };
         }
 
-        public string ConsumerKey
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public Version ConsumerVersion
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public DateTime CreatedOn
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string Token
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string VerificationCode
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        internal static IServiceProviderRequestToken FromDataRequestToken(global::Glipho.OAuth.Providers.Database.RequestToken requestToken)
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Convert this request token into a database request token.
+        /// </summary>
+        /// <returns>The database request token created from this request token.</returns>
         internal Database.RequestToken ToDataRequestToken()
         {
-            throw new NotImplementedException();
+            return new Database.RequestToken
+            {
+                Authorised = false,
+                Callback = this.Callback,
+                ConsumerKey = this.ConsumerKey,
+                ConsumerVersion = this.ConsumerVersion,
+                Created = this.CreatedOn.ToUniversalTime(),
+                Id = 0,
+                Scope = null,
+                Token = this.Token,
+                TokenSecret = null,
+                Username = null,
+            };
         }
     }
 }
