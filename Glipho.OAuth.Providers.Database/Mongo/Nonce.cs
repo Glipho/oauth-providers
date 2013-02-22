b98dd546a8dd2c7343fcfb79d85b1a5045e5f616
James Toyer <james@glipho.com>
2013-02-22 16:47:08 +0000

    Implemented the nonces database interface

---
+++ Glipho.OAuth.Providers.Database/Mongo/Nonce.cs
@@ -0,0 +1,125 @@
+﻿namespace Glipho.OAuth.Providers.Database.Mongo
+{
+    using System;
+    using System.Diagnostics;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    /// <summary>
+    /// Mongo nonce representation.
+    /// </summary>
+    [DebuggerDisplay("{ToString}"), BsonIgnoreExtraElements]
+    public class Nonce
+    {
+        /// <summary>
+        /// Gets or sets the identifier of the nonce.
+        /// </summary>
+        [BsonId]
+        public BsonObjectId Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the context.
+        /// </summary>
+        [BsonRequired]
+        public string Context { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code.
+        /// </summary>
+        [BsonRequired]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the issued date of the nonce.
+        /// </summary>
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime Issued { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expiry date of the nonce.
+        /// </summary>
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime Expires { get; set; }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents the current <see cref="Nonce"/>.
+        /// </summary>
+        /// <returns>A <see cref="System.String"/> that represents the current <see cref="Nonce"/>.</returns>
+        public override string ToString()
+        {
+            return string.Format(
+                "{0} [{1}, {2}, {3}, {4}, {5}]",
+                this.GetType().Name,
+                this.Id,
+                this.Context,
+                this.Code,
+                this.Issued,
+                this.Expires);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a <see cref="Nonce"/>.
+        /// </summary>
+        /// <returns>A hash code for the current <see cref="Nonce"/>.</returns>
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to the current <see cref="Nonce"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with the current <see cref="Nonce"/>.</param>
+        /// <returns>
+        /// true if the specified <see cref="System.Object"/> is equal to the current <see cref="Nonce"/>; otherwise false.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Nonce);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Nonce"/> is equal to the current <see cref="Nonce"/>.
+        /// </summary>
+        /// <param name="nonce">The <see cref="Nonce"/> to compare with the current <see cref="Nonce"/>.</param>
+        /// <returns>
+        /// true if the specified <see cref="Nonce"/> is equal to the current <see cref="Nonce"/>; otherwise false.
+        /// </returns>
+        public bool Equals(Nonce nonce)
+        {
+            if (nonce == null)
+            {
+                return false;
+            }
+
+            return this.Context == nonce.Context
+                && this.Code == nonce.Code
+                && this.Id == nonce.Id
+                && this.Issued == nonce.Issued
+                && this.Expires == nonce.Expires;
+        }
+
+        /// <summary>
+        /// Create a new nonce from a database nonce.
+        /// </summary>
+        /// <param name="nonce">The database nonce to create a new nonce from.</param>
+        /// <returns>A new nonce created from the database nonce.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if a parameter is null.</exception>
+        internal static Nonce FromNonce(Database.Nonce nonce)
+        {
+            if (nonce == null)
+            {
+                throw new ArgumentNullException("nonce", "nonce is null");
+            }
+
+            return new Nonce
+            {
+                Code = nonce.Code,
+                Context = nonce.Context,
+                Expires = nonce.Expires.ToUniversalTime(),
+                Id = new BsonObjectId(nonce.Id.ToString()),
+                Issued = nonce.Issued.ToUniversalTime(),
+            };
+        }
+    }
+}
---
+++ Glipho.OAuth.Providers.Database/Mongo/Nonces.cs
@@ -0,0 +1,122 @@
+﻿namespace Glipho.OAuth.Providers.Database.Mongo
+{
+    using System;
+    using MongoDB.Driver;
+    using MongoDB.Driver.Builders;
+
+    /// <summary>
+    /// MongoDB implementation of the <see cref="INonces"/> database.
+    /// </summary>
+    public class Nonces : MongoDbClient, INonces
+    {
+        /// <summary>
+        /// The nonces collection name.
+        /// </summary>
+        private const string NoncesCollectionName = "nonces";
+
+        /// <summary>
+        /// Indicated if the indexes used by this class have been created.
+        /// </summary>
+        private static bool indexesCreated;
+
+        /// <summary>
+        /// The consumers collection.
+        /// </summary>
+        private MongoCollection<Nonce> noncesCollection;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Nonces"/> class.
+        /// </summary>
+        public Nonces()
+        {
+            this.InitialiseClass();
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Nonces"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        public Nonces(string connectionString)
+            : base(connectionString)
+        {
+            this.InitialiseClass();
+        }
+
+        /// <summary>
+        /// Adds a nonce to the database.
+        /// </summary>
+        /// <param name="nonce">Details of the nonce to add.</param>
+        /// <returns>true if nonce was successful added; else false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if a parameter is null.</exception>
+        /// <exception cref="Glipho.OAuth.OAuthException">Thrown if an error occurs while executing the requested command.</exception>
+        public bool Add(Database.Nonce nonce)
+        {
+            if (nonce == null)
+            {
+                throw new ArgumentNullException("nonce", "nonce is null");
+            }
+
+            try
+            {
+                var result = this.noncesCollection.Insert(nonce, WriteConcern.WMajority);
+                return result.Ok;
+            }
+            catch (MongoException ex)
+            {
+                throw new OAuthException("Unable to add nonce to the database. A database error has occurred.", ex, ErrorCode.Database);
+            }
+        }
+
+        /// <summary>
+        /// Remove all expired nonces from the database.
+        /// </summary>
+        /// <returns>true if removal was successful; else false.</returns>
+        /// <exception cref="Glipho.OAuth.OAuthException">Thrown if an error occurs while executing the requested command.</exception>
+        public bool RemoveExpired()
+        {
+            try
+            {
+                var query = Query<Nonce>.LT(n => n.Expires, DateTime.UtcNow);
+                var result = this.noncesCollection.Remove(query, RemoveFlags.None, WriteConcern.WMajority);
+                return result.Ok;
+            }
+            catch (MongoException ex)
+            {
+                throw new OAuthException("Unable to remove expired nonces. A database error has occurred.", ex, ErrorCode.Database);
+            }
+        }
+
+        /// <summary>
+        /// Initialise the class.
+        /// </summary>
+        private void InitialiseClass()
+        {
+            this.noncesCollection = this.InitialiseCollection<Nonce>(NoncesCollectionName);
+            this.EnsureIndexesExist();
+        }
+
+        /// <summary>
+        /// Ensure the relevant indexes exist.
+        /// </summary>
+        private void EnsureIndexesExist()
+        {
+            if (indexesCreated)
+            {
+                return;
+            }
+
+            lock (NoncesCollectionName)
+            {
+                if (indexesCreated)
+                {
+                    return;
+                }
+
+                var uniqueNonceIndex = IndexKeys<Nonce>.Ascending(n => n.Context).Ascending(n => n.Code).Ascending(n => n.Issued);
+                var indexOptions = IndexOptions.SetBackground(true).SetUnique(true);
+                this.noncesCollection.EnsureIndex(uniqueNonceIndex, indexOptions);
+                indexesCreated = true;
+            }
+        }
+    }
+}
