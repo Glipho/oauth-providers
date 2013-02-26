c0804245fe4e74662d50baaa85e4f53bab8cbe34
James Toyer <james@glipho.com>
2013-02-26 10:41:58 +0000

    Created project to bind interfaces using ninject

---
+++ Glipho.OAuth.Providers.Ninject/DefaultMongoBindings.cs
@@ -0,0 +1,20 @@
+﻿namespace Glipho.OAuth.Providers.Ninject
+{
+    using global::Ninject.Modules;
+
+    /// <summary>
+    /// Default MongoDB bindings for the <see cref="Glipho.OAuth.Providers"/> project.
+    /// </summary>
+    public class DefaultMongoBindings : NinjectModule
+    {
+        /// <summary>
+        /// Loads the module into the kernel. 
+        /// </summary>
+        public override void Load()
+        {
+            this.Bind<Database.IConsumers>().To<Database.Mongo.Consumers>();
+            this.Bind<Database.IIssuedTokens>().To<Database.Mongo.IssuedTokens>();
+            this.Bind<Database.INonces>().To<Database.Mongo.Nonces>();
+        }
+    }
+}
