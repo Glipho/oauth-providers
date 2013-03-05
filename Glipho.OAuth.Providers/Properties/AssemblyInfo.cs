17e76c8533828eac2020301951a849435a06758f
James Toyer <james@glipho.com>
2013-03-05 14:09:10 +0000

    Updated NuSpecs and version numbers

--- Glipho.OAuth.Providers/Properties/AssemblyInfo.cs
+++ Glipho.OAuth.Providers/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Glipho.OAuth.Providers")]
-[assembly: AssemblyDescription("Glipho's implementation of OAuth providers")]
+[assembly: AssemblyDescription("Glipho's implementation of OAuth providers, specifically OAuth 1.0.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Glipho Ltd")]
 [assembly: AssemblyProduct("Glipho.OAuth.Providers")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
